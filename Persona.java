fc664699521f30afeb9a6b2dbcca60c4ed5b0c3c
José Manuel <escribiendocodigo1@gmail.com>
2017-03-20 13:45:01 -0600

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,18 @@
+
+public class Persona{
+String nombre;
+int edad;
+char sexo;
+
+
+public Persona(){
+System.out.println("El objeto se ha creado");
+}
+
+public Persona(String nombre,int edad, char sexo){
+this.nombre=nombre;
+this.edad=edad;
+this.sexo=sexo;
+}
+
+}
